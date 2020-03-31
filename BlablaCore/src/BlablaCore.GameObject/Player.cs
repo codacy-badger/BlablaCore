ae7a5dfa848970110285690a3d67d477e6f5c7f8
Valwex <pierrecousteix93@gmail.com>
2020-03-31 21:53:58 +0200

    fix issues

---
+++ BlablaCore/src/BlablaCore.GameObject/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlablaCore.src.BlablaCore.GameObject
+{
+    public class Player
+    {
+        public string name { get; set; }
+        public int grade { get; set; }
+        public int xp { get; set; }
+        public int skin { get; set; }
+        public int sexe { get; set; }
+        public int uid { get; set; }
+        public int pid { get; set; }
+        public int lastMapId { get; set; }
+        public int time { get; set; }
+        public bool console { get; set; }
+        public bool logged { get; set; }
+        public int moderatorColor { get; set; }
+        public bool isModeratorColorEnabled { get; set; }
+
+    }
+}
