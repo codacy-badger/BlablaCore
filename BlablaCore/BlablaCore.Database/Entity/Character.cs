eae2dfa5339d04e81c9c629458795b5babf1db94
Valwex <pierrecousteix93@gmail.com>
2020-04-01 18:23:02 +0200

    create database

---
+++ BlablaCore/BlablaCore.Database/Entity/Character.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlablaCore.BlablaCore.Database.Entity
+{
+    public class Character
+    {
+        public long Bbl { get; set; }
+
+        [Key]
+        public long CharacterId { get; set; }
+        public string ChatColor { get; set; }
+        public bool Direction { get; set; }
+        public short Gender { get; set; }
+        public short MapId { get; set; }
+        public int MapX { get; set; }
+        public int MapY { get; set; }
+        public string Name { get; set; }
+        public int[] SkinColor { get; set; }
+        public short SkinId { get; set; }
+        public int Uid { get; set; }
+        public long Xp { get; set; }
+    }
+}
