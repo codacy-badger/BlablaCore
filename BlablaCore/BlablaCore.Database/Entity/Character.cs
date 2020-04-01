f3c7ea513bf4fcdbca1750c4542c2239182d789a
Valwex <pierrecousteix93@gmail.com>
2020-04-01 18:37:39 +0200

    fix database

--- BlablaCore/BlablaCore.Database/Entity/Character.cs
+++ BlablaCore/BlablaCore.Database/Entity/Character.cs
@@ -8,19 +8,29 @@ namespace BlablaCore.BlablaCore.Database.Entity
 
         [Key]
         public long CharacterId { get; set; }
+
         public string ChatColor { get; set; }
+
         public bool Direction { get; set; }
+
         public short Gender { get; set; }
+
         public short MapId { get; set; }
+
         public int MapX { get; set; }
+
         public int MapY { get; set; }
 
         [Required]
         [MaxLength(25)]
         public string Name { get; set; } = "";
+
         public int[] SkinColor { get; set; }
+
         public short SkinId { get; set; }
+
         public int Uid { get; set; }
+
         public long Xp { get; set; }
     }
 }
