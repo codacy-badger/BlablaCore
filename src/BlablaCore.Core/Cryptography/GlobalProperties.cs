102640784e87234c91b10be3ccec299dcc6423d5
Valwex <pierrecousteix93&gmail.com>
2020-04-02 01:53:40 +0200

    fix issues

--- src/BlablaCore.Core/Cryptography/GlobalProperties.cs
+++ src/BlablaCore.Core/Cryptography/GlobalProperties.cs
@@ -2,60 +2,60 @@
 {
     public static class GlobalProperties
     {
-        public const short BIT_TYPE = 5;
+        public const short BitType = 5;
 
-        public const short BIT_STYPE = 5;
+        public const short BitStype = 5;
 
-        public const short BIT_MAP_ID = 12;
+        public const short BitMapId = 12;
 
-        public const short BIT_MAP_FILEID = 12;
+        public const short BitMapFileId = 12;
 
-        public const short BIT_MAP_REGIONID = 4;
+        public const short BitMapRegionId = 4;
 
-        public const short BIT_MAP_PLANETID = 4;
+        public const short BitMapPlanetId = 4;
 
-        public const short BIT_SWF_TYPE = 2;
+        public const short BitSwfType = 2;
 
-        public const short BIT_ERROR_ID = 5;
+        public const short BitErrorId = 5;
 
-        public const short BIT_CAMERA_ID = 9;
+        public const short BitCameraId = 9;
 
-        public const short BIT_USER_ID = 24;
+        public const short BitUserId = 24;
 
-        public const short BIT_USER_PID = 24;
+        public const short BitUserPid = 24;
 
-        public const short BIT_METHODE_ID = 6;
+        public const short BitMethodeId = 6;
 
-        public const short BIT_FX_ID = 6;
+        public const short BitFxId = 6;
 
-        public const short BIT_FX_SID = 16;
+        public const short BitFxSid = 16;
 
-        public const short BIT_FX_OID = 2;
+        public const short BitFxOid = 2;
 
-        public const short BIT_SKIN_ID = 10;
+        public const short BitSkinId = 10;
 
-        public const short BIT_TRANSPORT_ID = 10;
+        public const short BitTransportId = 10;
 
-        public const short BIT_SMILEY_ID = 6;
+        public const short BitSmileyId = 6;
 
-        public const short BIT_SMILEY_PACK_ID = 5;
+        public const short BitSmileyPackId = 5;
 
-        public const short BIT_GRADE = 10;
+        public const short BitGrade = 10;
 
-        public const short BIT_SKIN_ACTION = 3;
+        public const short BitSkinAction = 3;
 
-        public const short BIT_SERVER_ID = 2;
+        public const short BitServerId = 2;
 
-        public const short BIT_CHANNEL_ID = 16;
+        public const short BitChannelId = 16;
 
-        public const short BIT_APPARITION = 3;
+        public const short BitApparition = 3;
 
-        public const short BIT_PRISON = 6;
+        public const short BitPrison = 6;
 
-        public const short BIT_PARADIS = 7;
+        public const short BitParadis = 7;
 
-        public const short BIT_ELECTROCUTE = 9;
+        public const short BitElectrocute = 9;
 
-        public const short BIT_TELEPORT = 4;
+        public const short BitTeleport = 4;
     }
 }
